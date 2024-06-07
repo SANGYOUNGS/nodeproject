84bf8fad54c9c5e622d26b967f8e1f6bbd3c0a9b
김정연 <dlstod701@naver.com>
2024-06-07 14:24:46 +0900

    내용추가

---
+++ index.js
@@ -0,0 +1,9 @@
+import 'dotenv/config'
+import { app } from "./src/app";
+
+//.env 파일 상의 환경변수를 사용하며,만약에 없을 시 기본값으로 5000을 할당함. 
+const PORT = process.env.SERVER_PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${PORT}`);
+});

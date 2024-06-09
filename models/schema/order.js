32ed898291016ba7160a491a0f051786e4865e45
[IoT3] 정다은 <rose1gls@naver.com>
2024-06-09 17:05:31 +0900

    'models/schema/order.js'

--- models/schema/order.js
+++ models/schema/order.js
@@ -4,63 +4,63 @@ import { variantSchema } from "./product.js"; // variantSchema를 import
 const { Schema } = mongoose;
 
 const orderSchema = new Schema({
-    items: [
-        {
-            item: {
-                type: variantSchema, // variantSchema를 여기서 사용
-                required: true
-            },
-            stock : {
-                type: Number,
-                required: true
-            },
-        }
-    ],
-    customerId: {
-        type: String,
-        required: true
-    },
-    name: {
-        type: String,
-        required: true
-    },
-    address: {
-        type: new Schema(
-          {
-            postalCode: {
-                type: String,
-                required: true
-            },
-            address1: {
-                type: String,
-                required: true
-            },
-            address2: {
-                type: String,
-                required: false
-            }
-          },
-          {
-            _id: false,
-          }
-        ),
+  items: [
+    {
+      item: {
+        type: variantSchema, // variantSchema를 여기서 사용
         required: true,
-    },
-    phone: {
-        type: String,
+      },
+      quantity: {
+        type: Number,
         required: true,
+      },
     },
-    orderDate: { // 변경된 필드 이름
-        type: Date,
-        required: true,
-    },
-    orderState: { // 변경된 필드 이름
-        type: String,
-        required: true,
-    }
+  ],
+  customerId: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: new Schema(
+      {
+        postalCode: {
+          type: String,
+          required: true,
+        },
+        address1: {
+          type: String,
+          required: true,
+        },
+        address2: {
+          type: String,
+          required: false,
+        },
+      },
+      {
+        _id: false,
+      }
+    ),
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  orderDate: {
+    // 변경된 필드 이름
+    type: Date,
+    required: true,
+  },
+  orderState: {
+    // 변경된 필드 이름
+    type: String,
+    required: true,
+  },
 });
-
-
 
 const Order = mongoose.model("Order", orderSchema);
 

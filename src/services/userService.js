c2a759486c5e2bb54fa0834b02e3283b829c6dd3
김정연 <dlstod701@naver.com>
2024-06-10 14:26:33 +0900

    컨트롤러 수정

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,142 +1,118 @@
-import { UserModel } from "../db"; // from을 폴더(db) 로 설정 시, 디폴트로 index.js 로부터 import함.
+import User from '../models/schema/user.js';
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-class UserService {
-
-  constructor(userModel) {
-    this.userModel = userModel;
-  }
-
-    //로그인
-    async getUserToken(loginInfo) {
-      try{
-      const { email, password } = loginInfo;
-
-      const user = await this.userModel.findByEmail(email);
-      if(!user) {
-        throw new Error('해당 이메일은 가입내역이 없습니다.');
-      }
-
-      //비밀번호 일치여부
-      const correctPasswordHash = user.password;
-
-      const isPasswordCorrect = await bcrypt.compare(
-        password,
-        correctPasswordHash
-      );
-
-      if(!isPasswordCorrect) {
-        throw new Error('비밀번호가 일치하지 않습니다.');
-      };
-
-    //로그인 성공 => 웹토큰 생성
-    const secretKey = process.env.JWT_SECRET_KEY || 'secret-key';
-
-    const token = jwt.sign({ userId: user.id, role: user.role }, secretKey);
-
-    return { token };
-  }catch(err) {
-    throw err;
-  }
-}
-
-  //사용자 목록
-  async getUsers() {
+  //로그인
+  const signIn = async (req, res, next) => {
     try{
-    const users = await this.userModel.findAll();
-    if(!users) {
-      throw new Error('해당 사용자를 찾을 수 없습니다.');
-    }
-    return users;
-  }catch(err) {
-    throw err;
-  }
-  }
-
-  //개별 사용자(수정페이지)
-  async getUser(email) {
-    try{
-    const user = await this.userModel.findByEmail(email);
-    if(!user) {
-      throw new Error('해당 사용자를 찾을 수 없습니다.');
-    }
-    return user;
-  }catch(err) {
-    throw err;
-  }
-}
-
-  //유저정보 수정(비밀번호 소유시)
-  async setUser(userInfoRequired, toUpdate) {
-    try{
-    const { email, currentPassword } = userInfoRequired;
-
-    let user = await this.userModel.findByEmail(email);
+      const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
     if(!user) {
-      throw new Error('해당 사용자를 찾을 수 없습니다.');
+     const error = new Error('잘못된 이메일 또는 비밀번호입니다.');
+     error.statusCode = 401;
+     throw error;
     }
 
-    //비밀번호 일치 여부확인
-    const correctPasswordHash = user.password;
     const isPasswordCorrect = await bcrypt.compare(
-      currentPassword,
-      correctPasswordHash
+      password,
+      user.password
     );
-
     if(!isPasswordCorrect) {
-     throw new Error('비밀번호가 일치하지 않습니다.');
+    const error = new Error('잘못된 이메일 또는 비밀번호입니다.');
+    error.statusCode = 401;
+    throw error;
     }
-
-    //비밀번호 변경 해쉬화
-    const { password } = toUpdate;
-
-    if(password) {
-      const newPasswordHash = await bcrypt.hash(password, 10);
-      toUpdate.password = newPasswordHash;
-    }
-
-    //업데이트
-    user = await this.userModel.update({
-      email,
-      update: toUpdate,
-    });
-
-    return user;
-  }catch(err) {
-    throw err;
-  }
+    const secretKey = process.env.JWT_SECRET_KEY || 'jwt-secret-key';
+    const token = jwt.sign({ userId: user._id, role: user.role }, secretKey);
+    res.status(200).json({ token });
+  } catch(err) {
+  }next(err);
 }
 
-  //사용자 정보 삭제
-  async deleteUser(userInfoRequired) {
-    try{
-    const { email, currentPassword } = userInfoRequired;
-
-    let user = await this.userModel.findByEmail(email);
+  //사용자 조회
+  const getUser = async (req, res, next) => {
+    try {
+      const userId = req.params.id;
+      const user = await User.findById(userId);
 
     if(!user) {
-      throw new Error('해당 사용자를 찾을 수 없습니다.');
-    };
-
-  const correctPasswordHash = user.password;
-
-  const isPasswordCorrect = await bcrypt.compare(
-    currentPassword,
-    correctPasswordHash
-  );
-
-  if(!isPasswordCorrect) {
-    throw new Error('비밀번호가 일치하지 않습니다. 다시 한 번 확인해 주세요.');
+      const error = new Error('사용자를 찾을 수 없습니다.');
+      error.statusCode = 404;
+      throw error;
+    }
+    res.status(200).json(user);
+  } catch(err) {
+    next(err);
   }
-  await this.userModel.delete(email);
-  return user;
-}catch(err) {
-  throw err;
-}}
 };
 
-const userService = new UserService(UserModel);
+  //유저정보 수정
+  const updateUser = async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const updateData = req.body;
 
-export { userService };
+      const user = await User.findById(id);
+
+      if(!user) {
+        const error = new Error('사용자를 찾을 수 없습니다.');
+        error.statusCode = 404;
+        throw error;
+      }
+
+      if(updateData.currentPassword && updateData.newPassword) {
+        const isPasswordCorrect = await bcrypt.compare(updateData.currentPassword, user.password);
+        
+        if(!isPasswordCorrect) {
+          const error = new Error('비밀번호가 일치하지 않습니다.');
+          error.statusCode = 400;
+          throw error;
+      }
+
+      const newPasswordHash = await bcrypt.hash(updateData.newPassword, 10);
+      updateData.password = newPasswordHash;
+
+      delete updateData.currentPassword;
+      delete updateData.newPassword;
+    }
+    const updatedUser = await User.findByIdAndUpdate(id, updateData, { new: true });
+
+    res.status(200).json(updatedUser);
+    }catch(err) {
+      next(err);
+    }
+    };
+
+  //사용자 정보 삭제
+  const deleteUser = async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = await User.findByIdAndDelete(id);
+      if (!user) {
+        const error = new Error('사용자를 찾을 수 없습니다.');
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).send('성공적으로 삭제되었습니다.');
+    }catch(err) {
+      next(err);
+    }
+  };
+
+    //로그아웃
+    const logout = async (res, req, next) => {
+      try {
+        res.status(200).send('성공적으로 로그아웃되었습니다.');
+      }catch(err) {
+        next(err);
+      }
+    };
+
+export default {
+  signIn,
+  getUser,
+  updateUser,
+  deleteUser,
+  logout,
+}

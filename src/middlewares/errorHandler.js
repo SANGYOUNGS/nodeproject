b72303b16f413e0e4097d2a7ff5cb4cdc10e599c
김정연 <dlstod701@naver.com>
2024-06-10 14:26:56 +0900

    에러핸들러 미들웨어 추가

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,6 @@
+function errorHandler(err, req, res, next) {
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({ message: err.message || 'server error'});
+}
+
+export default errorHandler;

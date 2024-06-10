36d477b4609d84883cdf50fad04e5a7e402fd211
김정연 <dlstod701@naver.com>
2024-06-10 21:44:52 +0900

    라우터 수정

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,5 @@ const router = express.Router();
 router.get('/user/:id', userService.getUser);
 router.put('/user/:id', userService.updateUser);
 router.delete('/user/:id', userService.deleteUser);
-router.post('/login', userService.signIn);
-router.post('/logout', userService.logout);
 
 export default router;

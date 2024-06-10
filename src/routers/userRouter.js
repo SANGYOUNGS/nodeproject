c76d7acac2ae6a908d8aa68db85b5ff9cf1eeccc
김정연 <dlstod701@naver.com>
2024-06-10 16:02:17 +0900

    라우터 수정

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,15 +1,12 @@
 import express from 'express';
 import userService  from '../services/userService.js';
-import errorHandler from '../middlewares/errorhandler.js';
 
 const router = express.Router();
 
 router.get('/user/:id', userService.getUser);
 router.put('/user/:id', userService.updateUser);
-router.delete('/user/:id', deleteUser);
+router.delete('/user/:id', userService.deleteUser);
 router.post('/login', userService.signIn);
 router.post('/logout', userService.logout);
 
-router.use(errorHandler);
-
-export default app;
+export default router;

99a8e94833d8304a1dc609416f03b259cb6a17a8
[IOT3] 이상영 <sy2756069@gmail.com>
2024-06-11 18:11:52 +0900

    미들웨어 추가

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,12 @@
 import express from "express";
 import productService from "../services/adminService.js";
-
+import { authenticationMiddleware, checkRole } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 
 // 제품 추가
-router.post('/api/admin/products' , async (req, res) => {
+router.post('/api/admin/products',authenticationMiddleware, checkRole, async (req, res) => {
   try {
     const newProduct = await productService.addProduct(req.body);
     res.status(201).json(newProduct);
@@ -16,7 +16,7 @@ router.post('/api/admin/products' , async (req, res) => {
 });
 
 // 제품 수정
-router.put('/api/admin/products/:id' , async (req, res) => {
+router.put('/api/admin/products/:id' ,authenticationMiddleware, checkRole, async (req, res) => {
   try {
     const updatedProduct = await productService.updateProduct(req.params.id, req.body);
     if (!updatedProduct) {
@@ -29,7 +29,7 @@ router.put('/api/admin/products/:id' , async (req, res) => {
 });
 
 // 제품 삭제
-router.delete('/api/admin/products/:id', async (req, res) => {
+router.delete('/api/admin/products/:id', authenticationMiddleware, checkRole, async (req, res) => {
   try {
     const deletedProduct = await productService.deleteProduct(req.params.id);
     if (!deletedProduct) {

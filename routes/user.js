e5ac36ad7bc6cc993e9ecbed3ff0d8d15467397e
Lee gangho <rkdgh1190@gmail.com>
2024-06-14 20:34:33 +0900

    Revert "오타수정"
    This reverts commit f221e598cf1efdc52c6e5495fd5b13b4c8b91105.

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 import express from "express";
 import User from "../models/schema/user.js";
-import bcrypt from "bcrypt";
+import bcrypt from 'bcrpyt';
 import { authenticationMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -24,61 +24,62 @@ router.get("/me", authenticationMiddleware, async (req, res, next) => {
 });
 
 // 정보 수정
-router.put("/user/:id", authenticationMiddleware, async (req, res, next) => {
-  const userId = req.user.id;
-  if (!userId) {
-    const error = new Error("사용자를 찾을 수 없습니다.");
-    error.statusCode = 404;
-    return next(error);
-  }
-  const { name, email, password, address, phoneNumber } = req.body;
-  try {
-    const updateData = { name, email, address, phoneNumber };
-    if (password) {
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
-      updateData.password = hashedPassword;
-    }
-    const updateUser = await User.findByIdAndUpdate(userId, updateData, {
-      new: true,
-    });
-    if (!updateUser) {
+router.put("/user/:id",  authenticationMiddleware, async (req, res, next) => {
+    const userId = req.user.id;
+    if(!userId) {
       const error = new Error("사용자를 찾을 수 없습니다.");
       error.statusCode = 404;
-      throw error;
+      return next(error);
     }
-    res.json({
-      message: "사용자 정보가 업데이트되었습니다.",
-      user: updateUser,
-    });
-  } catch (err) {
-    next(err);
-  }
-});
+    const { name, email, password, address, phoneNumber } = req.body;
+    try {
+      const updateData = {name, email, address, phoneNumber};
+      if(password) {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        updateData.password  = hashedPassword;
+      }
+      const updateUser = await User.findByIdAndUpdate(
+        userId,
+        updateData,
+        {new: true}
+      );
+      if(!updateUser) {
+        const error = new Error("사용자를 찾을 수 없습니다.");
+        error.statusCode = 404;
+        throw error;
+      }
+      res.json({message: "사용자 정보가 업데이트되었습니다.", user: updateUser});
+    } catch(err) {
+      next(err);
+    }
+  });
 
 // 정보 삭제
-router.delete("/:id", authenticationMiddleware, async (req, res, next) => {
-  const userId = req.user.id;
+router.delete(
+  "/:id", authenticationMiddleware,
+  async (req, res, next) => {
+    const userId = req.user.id;
 
-  if (!userId) {
-    const error = new Error("사용자를 찾을 수 없습니다.");
-    error.statusCode = 404;
-    return next(error);
-  }
-
-  try {
-    const deletedUser = await User.findByIdAndDelete(userId);
-
-    if (!deletedUser) {
+    if(!userId) {
       const error = new Error("사용자를 찾을 수 없습니다.");
       error.statusCode = 404;
-      throw error;
+      return next(error);
     }
 
-    res.json({ message: "사용자 정보가 삭제되었습니다." });
-  } catch (err) {
-    next(err);
-  }
-});
+    try {
+      const deletedUser = await User.findByIdAndDelete(userId);
+
+      if(!deletedUser) {
+        const error = new Error("사용자를 찾을 수 없습니다.");
+        error.statusCode = 404;
+        throw error;
+      }
+
+      res.json({message: "사용자 정보가 삭제되었습니다."});
+    } catch(err) {
+      next(err);
+    }
+  });
 
 export default router;

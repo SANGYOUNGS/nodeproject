f6ba1351e8527c48ae9d2d543689dd6dc4fd2188
김정연 <dlstod701@naver.com>
2024-06-14 23:06:47 +0900

    오타 수정

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,17 +1,17 @@
-import jwt from 'jsonwebtoken'; 
+import jwt from 'jsonwebtoken';
 
-const authenticationMiddleware =(req, res, next) => {
+const authenticationMiddleware = (req, res, next) => {
   const token = req.cookies.adminCookie ?? req.cookies.userCookie;
   if (!token) {
     return res.status(401).json({ message: "토큰이 없습니다." });
   }
   
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if(err) {
+    if (err) {
       if (err.name === "TokenExpiredError") {
         return res
-        .status(401)
-        .json({ message: "토큰이 만료되었습니다. 다시 로그인 해주세요." });
+          .status(401)
+          .json({ message: "토큰이 만료되었습니다. 다시 로그인 해주세요." });
       }
       if (err.name === "JsonWebTokenError") {
         return res
@@ -20,19 +20,21 @@ const authenticationMiddleware =(req, res, next) => {
       }
       return next(err);
     }
-      if(decoded.role === 'admin') {
-        req.user = decoded;
-        next();
-      } else {
-        return res.status(403).json({ message: '접근 권한이 없습니다.'});
-      }
-      if(decoded.role === 'user') {
-        req.user = decoded;
-        next();
-      } else {
-        return res.status(403).json({message: '접근 권한이 없습니다.'});
-      }
-    });
-  };
 
-export default authenticationMiddleware;
+    // 인증된 사용자의 역할에 따라 권한 부여
+    if (decoded.role === 'admin') {
+      req.user = decoded;
+      next();
+    } else {
+      return res.status(403).json({ message: '접근 권한이 없습니다.' });
+    }
+    if (decoded.role === 'user') {
+      req.user = decoded;
+      next();
+    } else {
+      return res.status(403).json({ message: '접근 권한이 없습니다.' });
+    }
+  });
+};
+
+export { authenticationMiddleware };
